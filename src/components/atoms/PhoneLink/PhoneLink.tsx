5d67c717aa87259633fe8b28e65e7f2dc47fc851
Mielsen_9 <142320197+Mielsen9@users.noreply.github.com>
2025-08-26 21:12:01 +0300

    create PhoneLink

---
+++ src/components/atoms/PhoneLink/PhoneLink.tsx
@@ -0,0 +1,19 @@
+import * as s from './PhoneLink.module.scss'
+
+// Type
+interface Props {
+
+}
+
+// PhoneLink
+const PhoneLink: React.FC<Props> = (props) => {
+  const {} = props
+
+  return (
+    <div className={s.PhoneLink}>
+      PhoneLink
+    </div>
+  )
+}
+
+export default PhoneLink
