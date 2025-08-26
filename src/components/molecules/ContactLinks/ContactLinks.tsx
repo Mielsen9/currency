76c15a91900cc695724ca2e9c5e224b87126346d
Mielsen_9 <142320197+Mielsen9@users.noreply.github.com>
2025-08-26 21:08:38 +0300

    create ContactLinks

---
+++ src/components/molecules/ContactLinks/ContactLinks.tsx
@@ -0,0 +1,19 @@
+import * as s from './ContactLinks.module.scss'
+
+// Type
+interface Props {
+
+}
+
+// ContactLinks
+const ContactLinks: React.FC<Props> = (props) => {
+  const {} = props
+
+  return (
+    <div className={s.ContactLinks}>
+      ContactLinks
+    </div>
+  )
+}
+
+export default ContactLinks
