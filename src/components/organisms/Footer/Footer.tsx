6c5457ae37cabc5b072506f15e02b1dcb36aee44
Mielsen_9 <142320197+Mielsen9@users.noreply.github.com>
2025-08-27 21:01:47 +0300

    add a description to the Footer

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -10,9 +10,15 @@ const Footer: React.FC<Props> = (props) => {
   const {} = props
 
   return (
-    <div className={s.Footer}>
-      Footer
-    </div>
+    <h5 className={s.Footer}>
+      <div className={s.FooterDescription}>
+        <p>Калькулятор для обміну валют.</p>
+        <p>Надійність та конфіденційність.</p>
+        <p>© 2023-2024</p>
+        <p>Усі права застережені.</p>
+      </div>
+      <p>Currency Calculator</p>
+    </h5>
   )
 }
 
